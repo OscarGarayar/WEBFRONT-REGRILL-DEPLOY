38bd3a49cb9ebbdbb7f4ad655992bfd87bb03600
Oscar Nathaniel Garayar Mori <102541011+OscarGarayar@users.noreply.github.com>
2024-11-18 18:01:15 -0500

    Menu update

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -7,13 +7,32 @@ const model = ref([
     {
         label: 'Menu',
         items: [
-            { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
-            { label: 'Order List', icon: 'pi pi-fw pi-table', to: '/supplier/orderlist' },
+            { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/supplier/dashboard' },
             { label: 'Notifications', icon: 'pi pi-fw pi-bell', to: '/supplier/notifications' },
             { label: 'Profile', icon: 'pi pi-fw pi-user', to: '/supplier/profile' },
-
-
-
+            { label: 'Landing-Page', icon: 'pi pi-fw pi-desktop', to: '/landing' }
+        ]
+    },
+    {
+        label: 'Admin',
+        items: [
+            { label: 'Bills', icon: 'pi pi-fw pi-book', to: '/manager/bills' },
+            { label: 'Inventory', icon: 'pi pi-fw pi-table', to: '/admin/inventory' },
+            { label: 'Orders', icon: 'pi pi-fw pi-book', to: '/orders' },
+            { label: 'Recipes', icon: 'pi pi-fw pi-book', to: '/recipe' }
+        ]
+    },
+    {
+        label: 'Supplier',
+        items: [
+            { label: 'Order List', icon: 'pi pi-fw pi-table', to: '/supplier/orderlist' },
+            { label: 'New Orders', icon: 'pi pi-fw pi-table', to: '/supplier/new-orders' },
+        ]
+    },
+    {
+        label: 'Auth',
+        items: [
+            { label: 'Log-Out', icon: 'pi pi-fw pi-sign-in', to: '/login' }
         ]
     }/*,
     {
