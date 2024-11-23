006bd8fdbf3f97ac7a1d32005c1bdcb0c0043530
Oscar Nathaniel Garayar Mori <102541011+OscarGarayar@users.noreply.github.com>
2024-11-22 21:38:06 -0500

    suppliers

--- src/suppliers/order-management/order-list/services/supplier-new_order-api.service.js
+++ src/suppliers/order-management/order-list/services/supplier-new_order-api.service.js
@@ -1,13 +1,13 @@
 
-import {SupplierOrder} from '@/suppliers/order-management/order-list/model/supplier-order.entity';
+import {SupplierNewOrder} from '@/suppliers/order-management/order-list/model/supplier-new_order.entity';
 import axios from 'axios';
 
-export class SupplierOrderApiService{
+export class SupplierNewOrderApiService{
     async getSupplierOrder(){
-        const url = 'http://localhost:5086/api/v1/supplierorder'
+        const url = 'http://localhost:3000/SupplierNewOrder'
         const response = await axios.get(url);
         return response.data.map(supplierOrderData =>{
-            return new SupplierOrder({
+            return new SupplierNewOrder({
                 id: supplierOrderData.id,
                 supplierId: supplierOrderData.supplierId,
                 orderDate: supplierOrderData.orderDate,
